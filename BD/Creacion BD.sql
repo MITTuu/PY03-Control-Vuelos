6ed9cad8cdf8ab928e2b3c85869b7f2567c6f387
Jozafath Perez <jozperez@estudiantec.cr>
2024-06-03 18:26:45 -0600

    Database changes

--- BD/Creacion BD.sql
+++ BD/Creacion BD.sql
@@ -5,59 +5,59 @@ USE AirlineControl;
 GO
 
 CREATE TABLE Airline (
-    idAirline INT PRIMARY KEY,
-    name VARCHAR(255),
-    motto VARCHAR(255)
+    idAirline INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    motto VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE City (
-    idCity INT PRIMARY KEY,
-    name VARCHAR(255)
+    idCity INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Brand (
-    idBrand INT PRIMARY KEY,
-    name VARCHAR(255)
+    idBrand INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Plane (
-    registrationNumber VARCHAR(50) PRIMARY KEY,
-    idAirline INT,
-    idBrand INT,
-    capacity INT,
+    registrationNumber VARCHAR(50) PRIMARY KEY NOT NULL,
+    idAirline INT NOT NULL,
+    idBrand INT NOT NULL,
+    capacity INT NOT NULL,
     FOREIGN KEY (idAirline) REFERENCES Airline(idAirline),
     FOREIGN KEY (idBrand) REFERENCES Brand(idBrand)
 );
 
 CREATE TABLE Passengers (
-    idPassenger INT PRIMARY KEY,
-    name VARCHAR(255),
-    lastName1 VARCHAR(255),
-    lastName2 VARCHAR(255),
-    email VARCHAR(255),
-    phoneNumber VARCHAR(50)
+    idPassenger INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    lastName1 VARCHAR(255) NOT NULL,
+    lastName2 VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phoneNumber VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Pilots (
-    idPilot INT PRIMARY KEY,
-    name VARCHAR(255),
-    lastName1 VARCHAR(255),
-    lastName2 VARCHAR(255),
-    email VARCHAR(255),
-    phoneNumber VARCHAR(50),
-    idAirline INT,
+    idPilot INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    lastName1 VARCHAR(255) NOT NULL,
+    lastName2 VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phoneNumber VARCHAR(50) NOT NULL,
+    idAirline INT NOT NULL,
     FOREIGN KEY (idAirline) REFERENCES Airline(idAirline)
 );
 
 CREATE TABLE Flight (
-    idFlight INT PRIMARY KEY,
-    idAirline INT,
-    idPilot INT,
-    departureDateTime DATETIME,
-    arrivalDateTime DATETIME,
-    departureCityId INT,
-    arrivalCityId INT,
-    cancelled BIT,
+    idFlight INT PRIMARY KEY NOT NULL,
+    idAirline INT NOT NULL,
+    idPilot INT NOT NULL,
+    departureDateTime DATETIME NOT NULL,
+    arrivalDateTime DATETIME NOT NULL,
+    departureCityId INT NOT NULL,
+    arrivalCityId INT NOT NULL,
+    cancelled BIT NOT NULL,
     FOREIGN KEY (idAirline) REFERENCES Airline(idAirline),
     FOREIGN KEY (idPilot) REFERENCES Pilots(idPilot),
     FOREIGN KEY (departureCityId) REFERENCES City(idCity),
@@ -65,23 +65,23 @@ CREATE TABLE Flight (
 );
 
 CREATE TABLE FlightPassengers (
-    idPassenger INT,
-    idFlight INT,
-    confirmed BIT,
+    idPassenger INT NOT NULL,
+    idFlight INT NOT NULL,
+    confirmed BIT NOT NULL,
     PRIMARY KEY (idPassenger, idFlight),
     FOREIGN KEY (idPassenger) REFERENCES Passengers(idPassenger),
     FOREIGN KEY (idFlight) REFERENCES Flight(idFlight)
 );
 
 CREATE TABLE Role (
-    idRole INT PRIMARY KEY,
-    name VARCHAR(255)
+    idRole INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE [User] (
-    idUser INT PRIMARY KEY,
-    email VARCHAR(255),
-    password VARCHAR(255),
-    idRole INT,
+CREATE TABLE AppUser (
+    idUser INT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    idRole INT NOT NULL,
     FOREIGN KEY (idRole) REFERENCES Role(idRole)
 );
